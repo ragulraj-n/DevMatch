7da68ba6b79c0d6ff3c81d66cef96532639339bd
ragulraj-n <ragulrajrealme@gmail.com>
2026-02-26 17:44:06 +0530

    Fix userName and password bcryption

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,31 +1,39 @@
 const User = require("../models/User")
-
+const bcrypt = require("bcrypt");
 const registerUser = async (req,res) => {
+    const saltRounds = 10; 
     try{
     const {firstName, lastName, email, password} = req.body;
     if(!firstName || !lastName || !email || !password){
         return res.status(400).json({message : "Require all fields data"});
     }
     // unique user
-    const existingUser = User.findOne({email});
+    const existingUser = await User.findOne({email});
+    console.log(existingUser);
     if(existingUser) return res.status(400).json({message: "User already exists"});
 
-    let userName = firstName+lastName;
-    const existingUserName = User.findOne({userName});
-    if(existingUserName){
-        userName = firstName + (1000 + Math.random() * 9000);
+    let userName = (firstName+lastName)
+                    .toLowerCase()
+                    .replace(/[^a-z0-9]/g, "");
+    
+    let existingUserName = await User.findOne({userName});
+    while(existingUserName){
+        userName = firstName + Math.floor(1000 + Math.random() * 9000);
+        existingUserName = await User.findOne({userName});
     }
 
-    // bcrypt password
+    const hashPassword = await bcrypt.hash(password,saltRounds);
+    
     // jwt token
 
         const user = await User.create({
         firstName,
         lastName,
         email,
-        password,
+        password:hashPassword,
         userName
     });
+
     res.status(200).json({
         message:"User created successfully",
         data: user,

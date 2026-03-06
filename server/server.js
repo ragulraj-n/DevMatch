afc7bfb9635aa6eee9f1a23848170e506c0aba6e
ragulraj-n <ragulrajrealme@gmail.com>
2026-03-06 23:47:31 +0530

    fix: auth router in server for better api design

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/",authRouter);
+app.use("/auth",authRouter);
 app.use("/user",userRouter);
 app.use("/",connectionRouter);
 

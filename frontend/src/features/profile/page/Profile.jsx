dd166bd5b1ce72134b43c0b9fd97a7960829e0e7
ragulraj-n <ragulrajrealme@gmail.com>
2026-03-21 20:16:41 +0530

    setup: component, pages & routes for profile

---
+++ frontend/src/features/profile/page/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserProfile from '../components/UserProfile';
+
+const Profile = () => {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  )
+}
+
+export default Profile;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes  } from 'react-router-dom'
 import AuthLayout from '../layouts/AuthLayout'
 import Login from '../features/auth/pages/Login'
 import Signup from '../features/auth/pages/Signup'
+import Profile from '../features/profile/page/profile'
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
         <Route index  path="/" element={<Navigate to="/login" replace />} />
         <Route path="login" element={<Login />}/>
         <Route path="signup" element={<Signup />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
   )

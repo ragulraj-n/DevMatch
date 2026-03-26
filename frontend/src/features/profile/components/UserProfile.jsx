c77efaece6b9bba98657d1ec8c50bc80300bef0d
ragulraj-n <ragulrajrealme@gmail.com>
2026-03-26 16:24:12 +0530

    feat: completed layout for the user profile

--- frontend/src/features/profile/components/UserProfile.jsx
+++ frontend/src/features/profile/components/UserProfile.jsx
@@ -1,36 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DiCode, DiGithubBadge} from "react-icons/di";
+import { TbWorld } from "react-icons/tb";
+import { FaLinkedin, FaLink, FaLaptopCode} from "react-icons/fa";
+import { GrFormNextLink } from "react-icons/gr";
+import { FaUserPlus } from "react-icons/fa6";
+
 
 const UserProfile = () => {
+
   return (
-    <div className='flex m-1'>
-        <div className='border w-[20%] h-screen shadow-md fixed'>
-          <img src="https://m.media-amazon.com/images/I/71Zg6RRQzsL._AC_UF894,1000_QL80_.jpg" alt="" className='w-[95%] mx-auto mt-2 h-[300px]'/>
-          <div className='flex flex-col ml-5 mt-2'>
-              <h3 className='font-semibold text-lg'>@username6</h3>
-              <p>Github</p>
-              <p>LinkedIn</p>
-              <p>Portfolio</p>
+    <div className="flex flex-col min-h-screen h-screen">
+      <div className="w-4/5 bg-gray-400 mx-auto mt-16 rounded-3xl pb-10 flex">
+        <div className="w-[28%] pl-14">
+          <img className="w-52 mt-10 border-4 border-blue-700 rounded-full" src="https://api.dicebear.com/7.x/avataaars/svg?seed=alex_dev&backgroundColor=0f172a&hairColor=2c1e0f&topChance=80&accessoriesChance=30&clothingColor=3b82f6" />
+          <div className="flex flex-col gap-5 mt-5 items-start">
+            <div className="flex gap-2 justify-center items-center">
+              <p className="flex h-8items-center border py-1 px-2 rounded-3xl bg-blue-400 font-semibold text-black text-[15px] gap-1"><DiCode size={25} />Senior Dev</p> 
+              <p className="border h-9 py-1 px-2 rounded-3xl bg-blue-400 font-semibold text-black text-[15px] flex items-center gap-1"
+              ><TbWorld />Lets Collab</p>
+            </div>
+            <h1 className="text-white font-bold text-[35px]">Ragul Raj</h1>
+            <button className="border w-4/5 h-10 flex justify-center items-center gap-2 bg-blue-600 rounded-full text-white text-lg
+            font-semibold"><FaUserPlus />Connect</button>
+            <div className="w-full flex justify-start items-center gap-5">
+              <DiGithubBadge size={40} />
+              <FaLinkedin size={30}/>
+              <FaLink size={30}/>
+
+            </div>
           </div>
         </div>
-        <div className='ml-[20%] flex flex-col items-start pl-3 pt-5 w-full mr-10'>
-            <div className='h-screen w-full'>
-              <div className='fixed'>
-                 <h1 className='font-semibold text-3xl'>Ragul Raj N</h1>
-                 <h3 className='text-sm'>coimbatore</h3>
-              </div>
-              <div className='mt-[8%] border h-1/3 w-full text-xl p-4'>
-                Driven Software Developer eager to turn complex problems into elegant solutions. Proficient in JavaScript, React, and Python, with a strong foundation in computer science principles. Quick learner focused on building high-quality, user-friendly applications. Ready to contribute and grow within a collaborative tech team.
-              </div>
-              <div className='border h-1/6 mt-10'>
-                  Skills: 
-              </div>
-              <div className='border h-1/6 mt-10'>
-                  Interests: 
-              </div>
-            </div>
+        <div className="w-[72%] flex flex-col gap-5 mt-10 pr-10">
+          <h2 className="text-[22px] font-bold text-black">About</h2>
+          <p className="text-white text-[17px] ">
+            Hello, I'm Ragul Raj passionate about building smart and scalable web & mobile applications. I've completed a full-stack development course and constantly explore new technologies to refine my skills. Focused on continuous learning, I aim to transition into the IT industry by 2027 and eventually move towards AI and data science.
+          </p>
+          <div className="h-0.5 bg-blue-600 w-full"/>
+          <h2 className="text-[22px] font-bold text-black">Skills</h2>
+          <div className="flex gap-2">
+            {
+              ["React.js","Node.js","Express.js","MongoDB","C++","TailwindCSS"].map(e =>
+                <p className="border rounded-lg px-3 py-1 bg-gray-300 text-black font-medium text-[16px]">{e}</p>
+              )
+            }
+          </div>
+          <div className="h-0.5 mt-3 bg-blue-600 w-full"/>
+          <h2 className="text-[22px] font-bold text-black">Interests</h2>
+           <div className="flex gap-2">
+            {
+              ["Web Development","Open Source","FinTech","App Development"].map(e =>
+                <p className="border rounded-lg px-4 py-1 bg-gray-300 text-black font-medium text-[16px]">{e}</p>
+              )
+            }
+          </div>
         </div>
+      </div>
+      <h1 className="ml-[10%] font-bold text-[25px] mt-10">Projects</h1>
+      <div className="pb-4">
+        <div className="w-4/5 bg-gray-400 mx-auto mt-4 rounded-xl py-2 pb-4">
+        <div className="px-5 py-2 flex gap-5 items-center">
+            <FaLaptopCode size={60}/>
+           <div className="w-3/5 h-30">
+             <h2 className="text-2xl font-bold mb-2">DevMatch</h2>
+             <p className="line-clamp-4">It is a web application which helps developer to connect with another developer to build something coolIt is a web application which helps developer to connect with another developer to build something coolIt is a web application which helps developer to connect with another developer to build something coolIt is a web application which helps developer to connect with another developer to build something cool</p>
+           </div>
+           <div className="flex flex-col mx-auto gap-6 items-center mt-5">
+              <p className="flex items-center border rounded-full py-1 px-3 bg-blue-700 text-black font-semibold cursor-pointer">Source Code<DiGithubBadge size={35} /></p>
+              <p className="flex items-center border rounded-full py-1 px-3 bg-blue-700 text-black font-semibold cursor-pointer">View Project<GrFormNextLink size={35} /></p>
+           </div>
+        </div>
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;

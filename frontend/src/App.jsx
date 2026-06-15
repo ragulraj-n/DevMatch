c9de5567aa6d8d1981122c2d158182a9804556ee
ragulraj-n <ragulrajrealme@gmail.com>
2026-06-15 14:01:40 +0530

    fix: appRoutes -> AppRoutes to deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Toaster} from 'react-hot-toast'
-import AppRoutes from './routes/appRoutes'
+import AppRoutes from './routes/AppRoutes'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import appStore from './app/appStore'
